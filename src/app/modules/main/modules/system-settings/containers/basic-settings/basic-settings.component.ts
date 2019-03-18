97e415663c49026e1a2c1275ec168af3947683d5
DongFangMingLiang <wangliang520wl@hotmail.com>
2019-03-18 19:08:36 +0800

    系统设置模块 优化

--- src/app/modules/main/modules/system-settings/containers/basic-settings/basic-settings.component.ts
+++ src/app/modules/main/modules/system-settings/containers/basic-settings/basic-settings.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {map} from 'rxjs/operators';
 
 @Component({
-  selector: 'app-basic-settings',
-  templateUrl: './basic-settings.component.html',
-  styleUrls: ['./basic-settings.component.scss']
+    selector: 'app-basic-settings',
+    templateUrl: './basic-settings.component.html',
+    styleUrls: ['./basic-settings.component.scss']
 })
 export class BasicSettingsComponent implements OnInit {
 
-  constructor() { }
+    constructor(private router: Router,
+                private activatedRoute: ActivatedRoute) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
